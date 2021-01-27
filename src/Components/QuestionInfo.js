5cc2f641950f570548687183aa40160d265cde96
Andres-XL <amovilla1103@gmail.com>
2021-01-26 19:29:55 -0500

    add progress bar

--- src/Components/QuestionInfo.js
+++ src/Components/QuestionInfo.js
@@ -3,16 +3,27 @@ import { ReactComponent as QuestionSVG } from "../Assets/question.svg";
 
 export function QuestionInfo( { category, curQuestion, maxQuestion } ) {
     return (
-        <div className={'flex-1 flex-row'}>
-            <div className={'flex-1 flex-row'}>
-                <QuestionSVG />
-                <span>{curQuestion + '/' + maxQuestion}</span>
+        <div className={'flex-1 flex-row progress-bar'}>
+            <div className={'flex-row progress-display'}
+                 style={{ '--percent' : (((curQuestion - 1) / maxQuestion) * 100) + '%' }}
+            >
+                <Contents category={category} curQuestion={curQuestion} maxQuestion={maxQuestion} />
             </div>
-            
-            <div className={'flex-3'}>
-                {category}
-            </div>
+            <Contents category={category} curQuestion={curQuestion} maxQuestion={maxQuestion} />
         </div>
     );
 }
 
+function Contents( { curQuestion, maxQuestion, category } ) {
+    
+    return <React.Fragment>
+        <div className={'flex-1 flex-row'}>
+            <QuestionSVG />
+            <span>{curQuestion + '/' + maxQuestion}</span>
+        </div>
+        
+        <div className={'flex-3 bold-text'}>
+            {category}
+        </div>
+    </React.Fragment>
+}
