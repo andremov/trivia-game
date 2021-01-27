9dc22b72ab9f507c2415c3d3db88b6f7b2082978
Andres-XL <amovilla1103@gmail.com>
2021-01-26 19:30:15 -0500

    made preamble card

--- src/Components/Cards/Preamble.js
+++ src/Components/Cards/Preamble.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from 'react';
 
-export function Preamble() {
+export function Preamble({startCallback}) {
     return (
         <Fragment>
-            <div className={'flex-3'}>
-                Welcome to trivia!
+            <div className={'flex-1 flex-center-all bold-text'}>
+                <span>Trivia Night!</span>
             </div>
+            <div className={'flex-3 flex-col flex-center-all'}>
+                <span>Ten questions.</span>
+                <span>Two options.</span>
+                <span>Twenty seconds.</span>
+                <span>Have you got what it takes?</span>
+            </div>
+            <button className={'primary start'} onClick={startCallback}>
+                Start!
+            </button>
         </Fragment>
     );
 }
